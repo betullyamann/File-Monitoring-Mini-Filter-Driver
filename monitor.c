f7bc6c2e9e3538351584cf9a63697fd5e17f8816
Betul YAMAN <betulyamann@gmail.com>
2025-03-07 11:24:50 +0300

    Remove header

--- monitor.c
+++ monitor.c
@@ -1,6 +1,25 @@
-#include "monitor.h"
+
+#include <fltKernel.h>
+#include <dontuse.h>
+#include <suppress.h>
 
 PFLT_FILTER RegisteredFilter;
+
+NTSTATUS DriverEntry(
+	_In_ PDRIVER_OBJECT DriverObject,
+	_In_ PUNICODE_STRING RegistryPath
+);
+
+FLT_PREOP_CALLBACK_STATUS PreDeleteDetectionCallback(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+);
+
+NTSTATUS FilterUnloadCallback(
+	FLT_FILTER_UNLOAD_FLAGS Flags
+);
+
 
 const FLT_OPERATION_REGISTRATION Callbacks[] = {
 	{IRP_MJ_SET_INFORMATION, 0, PreDeleteDetectionCallback, NULL},
