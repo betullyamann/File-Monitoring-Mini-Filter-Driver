63ee7107a2748c43c1f6c59571acfb88a6a484fa
Betul YAMAN <betulyamann@gmail.com>
2025-03-09 13:19:50 +0300

    Log the date, process id, operation type, and deleted file path on deletion.

--- monitor.c
+++ monitor.c
@@ -2,8 +2,21 @@
 #include <fltKernel.h>
 #include <dontuse.h>
 #include <suppress.h>
+#include <ntifs.h>
+#include <ntddk.h>
 
-PFLT_FILTER RegisteredFilter;
+#define EVENT_LOG_OPERATION_SIZE 16
+#define EVENT_LOG_FILE_PATH_SIZE 256
+#define EVENT_LOG_PROCESS_NAME_SIZE 256
+
+PFLT_FILTER RegisteredFilter = NULL;
+
+typedef struct EVENT_LOG {
+	LARGE_INTEGER Time;
+	ULONG ProcessId;
+	WCHAR Operation[EVENT_LOG_OPERATION_SIZE];
+	WCHAR FilePath[EVENT_LOG_FILE_PATH_SIZE];
+} DELETION_LOG;
 
 NTSTATUS DriverEntry(
 	_In_ PDRIVER_OBJECT DriverObject,
@@ -20,6 +33,16 @@ NTSTATUS FilterUnloadCallback(
 	FLT_FILTER_UNLOAD_FLAGS Flags
 );
 
+VOID LogDeletion(
+	_In_ PFLT_CALLBACK_DATA Data,
+	_Inout_ DELETION_LOG* deletionLog
+);
+
+VOID SafeCopy(
+	PWCHAR dest, 
+	PCWCHAR src, 
+	SIZE_T count
+);
 
 const FLT_OPERATION_REGISTRATION Callbacks[] = {
 	{IRP_MJ_SET_INFORMATION, 0, PreDeleteDetectionCallback, NULL},
@@ -58,6 +81,9 @@ FLT_PREOP_CALLBACK_STATUS PreDeleteDetectionCallback(
 			PFILE_DISPOSITION_INFORMATION fileInformation = (PFILE_DISPOSITION_INFORMATION)Data->Iopb->Parameters.SetFileInformation.InfoBuffer;
 			if (fileInformation->DeleteFile)
 			{
+				DELETION_LOG deletionLog = { 0 };
+				LogDeletion(Data, &deletionLog);
+
 				Data->IoStatus.Status = STATUS_ACCESS_DENIED;
 				DbgPrint("File deletion blocked.\n");
 				return FLT_PREOP_COMPLETE;
@@ -67,6 +93,39 @@ FLT_PREOP_CALLBACK_STATUS PreDeleteDetectionCallback(
 
 	return FLT_PREOP_SUCCESS_NO_CALLBACK;
 };
+
+VOID SafeCopy(PWCHAR dest, PCWCHAR src, SIZE_T count) {
+	wcsncpy(dest, src, count);
+	dest[count - 1] = L'\0';
+}
+
+VOID LogDeletion(
+	_In_ PFLT_CALLBACK_DATA Data,
+	_Inout_ DELETION_LOG* deletionLog)
+{
+	PFLT_FILE_NAME_INFORMATION fileNameInfo = NULL;
+
+	NTSTATUS status = FltGetFileNameInformation(Data, FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT, &fileNameInfo);
+	if (NT_SUCCESS(status))
+	{
+		status = FltParseFileNameInformation(fileNameInfo);
+		if (NT_SUCCESS(status))
+		{
+			KeQuerySystemTime(&deletionLog->Time);
+			deletionLog->ProcessId = FltGetRequestorProcessId(Data);
+			SafeCopy(deletionLog->Operation, L"delete", EVENT_LOG_OPERATION_SIZE);
+			SafeCopy(deletionLog->FilePath, fileNameInfo->Name.Buffer, EVENT_LOG_FILE_PATH_SIZE);
+		}
+		FltReleaseFileNameInformation(fileNameInfo);
+	}
+	else
+	{
+		KeQuerySystemTime(&deletionLog->Time);
+		deletionLog->ProcessId = FltGetRequestorProcessId(Data);
+		SafeCopy(deletionLog->Operation, L"delete", EVENT_LOG_OPERATION_SIZE);
+		SafeCopy(deletionLog->FilePath, L"unknown", EVENT_LOG_FILE_PATH_SIZE);
+	}
+}
 
 NTSTATUS FilterUnloadCallback(
 	FLT_FILTER_UNLOAD_FLAGS Flags) 
